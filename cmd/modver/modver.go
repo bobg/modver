5437f8d1ba068374d84dc1ea6944bdb74b8991e5
Bob Glickstein <bobg@emphatic.com>
2021-07-21 08:42:14 -0700

    Fix some doc.

--- cmd/modver/modver.go
+++ cmd/modver/modver.go
@@ -2,6 +2,12 @@
 // and tells whether a Major, Minor, or Patchlevel version bump
 // (or None)
 // is needed to go from one to the other.
+//
+// It takes two command-line arguments.
+// With `-git REPO` specified,
+// these are the SHA hashes of the older and newer commits in the specified Git repository.
+// Without `-git`,
+// these are the directories containing the older and newer versions of a module.
 package main
 
 import (
@@ -33,7 +39,7 @@ func main() {
 	}
 
 	if flag.NArg() != 2 {
-		fmt.Printf("Usage: %s -git OLDERSHA NEWERSHA\n", os.Args[0])
+		fmt.Printf("Usage: %s -git OLDERDIR NEWERDIR\n", os.Args[0])
 		os.Exit(1)
 	}
 	res, err := modver.CompareDirs(flag.Arg(0), flag.Arg(1))
